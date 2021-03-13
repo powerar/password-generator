e65941d6c37d39b577150a1633ea8fdb435d4e7d
powerar <arpower@bell.net>
2021-03-13 13:11:51 -0500

    added options and input validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,52 @@
 // Assignment code here
 
-//Generate string of characters based on the promptLength
 function generatePassword() {
-  var promptLength = prompt("How many characters should your password be? Please enter a number between 8 and 128.");
-  //var uppercase = alert("Should your password contain uppercase letters?");
-  //var lowercase = alert("Should your password contain lowercase letters?");
-  //var numbers = alert("Should your password contain numbers?");
-  //var special = alert("Should your password contain special characters?");
-  var password = '';
+  debugger;
+  var password = [''];
   var lettersLower = 'abcdefghijklmnopqrstuvwxyz';
   var lettersUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var numbers = '0123456789';
   var symbols = '!@#$%^&*';
-  for (i = 0; password.length < promptLength; i++) {
-    debugger;
-    password += lettersLower.charAt(Math.floor(Math.random() * lettersLower.length)) + 
-    numbers.charAt(Math.floor(Math.random() * numbers.length)) +
-    symbols.charAt(Math.floor(Math.random() * symbols.length)) +
-    lettersUpper.charAt(Math.floor(Math.random() * lettersUpper.length));
+  var promptLength = prompt("How many characters should your password be? Please enter a number between 8 and 128.");
+  if (promptLength < 8 || promptLength > 128) {
+    alert("You must enter a number between 8 and 128. Please try again.");
+    generatePassword();
+  } 
 
+  var confirmUppercase = confirm("Should your password contain uppercase letters?");
+  var confirmLowercase = confirm("Should your password contain lowercase letters?");
+  var confirmNumbers = confirm("Should your password contain numbers?");
+  var confirmSymbols = confirm("Should your password contain special characters?");
+  var generatedLower = '';
+  var generatedUpper = '';
+  var generatedNumbers = '';
+  var generatedSymbols = '';
+  for (i = 0; password.length < promptLength; i++) {
+    if (confirmLowercase) {
+      generatedLower += lettersLower.charAt(Math.floor(Math.random() * lettersLower.length));
+    }
+    if (confirmUppercase) {
+      generatedUpper += lettersUpper.charAt(Math.floor(Math.random() * lettersUpper.length));
+    }
+    if (confirmNumbers) {
+      generatedNumbers += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+    if (confirmSymbols) {
+      generatedSymbols += symbols.charAt(Math.floor(Math.random() * symbols.length));
+    }
+    password += generatedLower + generatedNumbers + generatedSymbols + generatedUpper;
+    }
     if (password.length >= promptLength) {
       var passwordMaxLength = password.length - promptLength;
       function removeCharacters() {
         password = password.substr(0, password.length - passwordMaxLength);
       }
-
       removeCharacters();
     }
-  }
-
   return password;
 };
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
